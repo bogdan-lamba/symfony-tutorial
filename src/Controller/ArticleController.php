48991da250404c601b01814878d0d160859e1b31
Bogdan L <bogdan.lamba89@gmail.com>
2018-10-23 13:48:34 +0300

    com

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('My homepage');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response("Future page for article: {$slug}");
+    }
+}
